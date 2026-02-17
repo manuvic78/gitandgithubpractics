920ccf63c27c54e2b1039ad4c532948b4cb8e222
manuvic78 <vitotobb07@gmail.com>
2026-02-16 21:45:19 -0400

    cambio de ejercicio

--- jueguitos/PATO.py
+++ jueguitos/PATO.py
@@ -1,16 +1,12 @@
 import random
 
-name_person= input("Hola, ¿cómo estás? El sistema quiere verificar tu archivos para saber si eres pato, Indica tu nombre : ")
+name_person= input("cara o sello?: ")
 
 num= random.randint(0,1)
 
 if num>0.5:
-    print(f"Felicidades {name_person}, tus archivos dicen que no eres pato 🦆")
-
-elif name_person == "victor" or name_person == "Victor":
-    
-    print("Acceso especial concedido, eres el creador del sistema 🦆.CLARAMENTE ERES EL HOMBRE MAS HETERO ")
+    print("Ha salido cara")
 
 else:
-    print(f"Felicidades {name_person}, tus archivos dicen PAAATOOOOO 🦆")
+    print("Ha salido sello ")
 
